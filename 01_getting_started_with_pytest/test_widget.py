4c856df29469cf3a3ac345ee99b7cea2e4a2e483
daczarne <daniel.czarnievicz@gmail.com>
2021-09-07 23:47:02 +0200

    getting started

---
+++ 01_getting_started_with_pytest/test_widget.py
@@ -0,0 +1,15 @@
+def test_widget_functions_as_expected():
+  assert True
+
+
+def test_widget_fails():
+  assert False
+
+
+def some_utils_function():
+  print("testing! testing! testing!")
+
+
+def test_another_passed_test():
+  assert True
+

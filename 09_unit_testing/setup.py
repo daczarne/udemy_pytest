2e982a5150c07166474e5cc7254fcfdca7e95118
daczarne <daniel.czarnievicz@gmail.com>
2021-09-23 19:48:59 +0200

    set up

---
+++ 09_unit_testing/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'unit_testing_with_pytest',
+  version = '0.1.0',
+  description = 'Change your text to title-case',
+  author = 'Brandon Blair (Udemy course)',
+  author_email = 'cbrandon.blair@gmail.com',
+  url = 'https://github.com/daczarne/udemy_pytest/09_unit_testing',
+  packages = find_packages()
+)
